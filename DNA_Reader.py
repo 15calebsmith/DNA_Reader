490e12f4b5e1d6e5634c76c9b28bc41925e1ed28
15calebsmith <smith.2026@wright.edu>
2017-03-15 16:21:07 -0400

    Adding basic user input through terminal/command line
    Basic logic for reading in arguments, testing provided path, and
    file extension testing. This is NOT complete, and still needs some other
    testing/varification for values. This just creates the basics.

--- DNA_Reader.py
+++ DNA_Reader.py
@@ -1,3 +1,69 @@
 import ReadFile as rf
+import os
+import sys
 
-rf.read_byte()
+########################################################################
+def fsa(path):
+    print '\t   ---fsa---'
+    print '\t>> fsa file found'
+    print '\t>> inPath       \t=', path
+
+def hid(path):
+    print '\t   ---testPath---'
+    print '\t>> hid file found'
+    print '\t>> inPath       \t=', path
+
+def testFile(inPath):
+    print '\t   ---testFile---'
+    #absInPath = os.path.abspath(inPath)
+    if os.path.isfile(inPath):
+        #print str(os.path.abspath(inPath)), 'is a file'
+        ext = os.path.splitext(inPath)[1]
+        print '\t>> ext          \t=', ext
+        if ext == '.fsa':
+            fsa(inPath)
+        elif ext == '.hid':
+            hid(inPath)
+
+def testPath(inPath, rec):
+    print '\t   ---testPath---'
+    if os.path.exists(inPath):
+        if os.path.isdir(inPath):
+            print '\t>> ', str(os.path.basename(inPath)), 'is a dir'
+            print '\t>> rec          \t=', rec
+            if rec == '1':
+                # recursive file reading logic from
+                # http://stackoverflow.com/questions/2212643/python-recursive-folder-read
+                for root, files in os.walk(inPath):
+                    print('\t>> root         \t=' + root)
+
+                    for filename in files:
+                        path = os.path.join(root, filename)
+                        testFile(path)
+            else:
+
+                print 'Directory found, but recursion is off.'
+        elif os.path.isfile(inPath):
+            testFile(inPath)
+    else:
+        print str(inPath), 'is not a dir or file'
+
+print '\t   ---start---'
+if len(sys.argv) > 3:
+    inPath = sys.argv[1]
+    outPath = sys.argv[2]
+    recurse = sys.argv[3]
+    normAbsInPath = os.path.normpath(os.path.abspath(inPath))
+
+    print '\t>> inpath       \t=', inPath
+    print '\t>> outpath      \t=', outPath
+    print '\t>> recurse      \t=', recurse
+    print '\t>> normAbsInPath\t=', normAbsInPath
+
+    testPath(normAbsInPath, recurse)
+
+else:
+    print 'Not enough arguments.'
+    print 'Format: inPath OutPath Recurse'
+
+print ''

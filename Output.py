7c5978a86677b7435bf7c6455b9c042d54c9fcda
Frank R <randazzofr@gmail.com>
2017-03-26 18:47:47 -0400

    Minor updates to the Output file

--- Output.py
+++ Output.py
@@ -2,20 +2,26 @@
 Collection of functions that convert a given data container to an text based output format
 """
 
-import random
-import types
 from xml.dom.minidom import getDOMImplementation
+import random  # Used only for testing func, remove in final
+import types   # Used only in assertions
 
+def toXML(tag_list, filename):
+    """
+    toXML(): Creates an XML-style document from the tags and their values
+    :param tag_list: A dictionary of lists of values
+    :param filename: Name of file to write to, no extension
+    :return: None
+    """
 
-def toXML(tag_list):
+    assert len(filename) > 0, \
+        "Filename cannot be empty."
     assert isinstance(tag_list, types.DictionaryType), \
         "Dictionary of lists required."
-    assert isinstance(tag_list[0], types.ListType), \
-        "The dictionary must contain lists."
 
     # Setup DOM Object and list
     impl = getDOMImplementation()
-    doc = impl.createDocument(None, "ABIF File", None)
+    doc = impl.createDocument(None, "ABIF", None)
     top_element = doc.documentElement
 
     # For each tag in the dict
@@ -27,19 +33,22 @@ def toXML(tag_list):
             text_item = doc.createTextNode(str(item))
             value_node.appendChild(text_item)
             new_elem.appendChild(value_node)
-
+        # Add the new element and its children to the tree
         top_element.appendChild(new_elem)
 
-    file = open('test.xml', 'wb')
-    doc.writexml(file, indent='\t\n', addindent='\t', encoding='utf-8')
+    # Output contents to file
+    filename += ".xml"
+    file_writer = open(filename, 'wb')
+    doc.writexml(file_writer, indent='\t\n', addindent='\t', encoding='utf-8')
+    file_writer.close()
 
 
 def testXML():
-    tag_list = {}
-    for i in range(10):
-        tag = []
-        for j in range(10):
-            tag.append(str(random.randrange(1, 100, 15)))
-        tag_list[i] = tag
+    tag_list = {"Tag1" : 0, "Tag2" : 0, "Tag3" : 0, "Tag4" : 0, "Tag5" : 0, "Tag6" : 0}
+    for tag in tag_list:
+        values = []
+        for val in range(10):  # Add 10 values to the tag's list of values
+            values.append(str(random.randrange(1, 100, 15)))  # Append random values
+        tag_list[tag] = values
 
-    toXML(tag_list)
+    toXML(tag_list, "test")
